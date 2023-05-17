813bbff7183bc7e80be5f07729a24938da074b17
Abdelkarim MNASRI <115982455+abdelkarim-MNASRI@users.noreply.github.com>
2023-05-17 14:50:12 +0200

    Add files via upload

---
+++ jour 1/job 1/var str.php
@@ -0,0 +1,10 @@
+JOB 1
+<?php
+$str = "LaPlateforme";
+echo $str; 
+
+$val = 6; 
+echo $val+ 4;
+
+$val = true;
+$val = false; 
---
+++ jour 1/job 2/index.php
@@ -0,0 +1,17 @@
+index 
+<?php 
+$str = "LaPlateforme";
+
+/* Affiche "Hello LaPlateforme!" */
+echo "Hello $str!";
+
+/*
+Affiche une ligne vide
+*/
+
+echo "\n";
+
+/*
+Affiche "Hello LaPlateforme!" à nouveau
+*/
+echo "Hello $str!";
